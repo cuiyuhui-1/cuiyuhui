dbc031fe6452e3d016a7ccd82b7c414fa33a3567
cuiyuhui-1 <56667327+cuiyuhui-1@users.noreply.github.com>
2019-10-23 16:33:41 +0800

    Update ceshi.java
    23

--- lianxi2019/src/lianxi2019/ceshi.java
+++ lianxi2019/src/lianxi2019/ceshi.java
@@ -3,7 +3,7 @@ package lianxi2019;
 public class ceshi {
 
 	public static void main(String[] args) {
-		String str = "Hello World!!!!!";
+		String str = "hi World!!!sd";
 		System.out.println(str);
 	}
 
